14d80f433c1b4f01105f77fe12d79b3c053cfa78
Nicolas Lou <nicolas931010@hotmail.com>
2019-03-12 14:51:39 -0400

    Added option to do general quality trimming

--- scripts/quality_filtering.sh
+++ scripts/quality_filtering.sh
@@ -5,6 +5,7 @@ SAMPLELIST=$1 # Path to a list of prefixes of the raw fastq files. It should be 
 SAMPLETABLE=$2 # Path to a sample table where the 1st column is the prefix of the raw fastq files. The 4th column is the sample ID, the 2nd column is the lane number, and the 3rd column is sequence ID. The combination of these three columns have to be unique. An example of such a sample table is: /workdir/cod/greenland-cod/sample_lists/sample_table_pe.tsv
 BASEDIR=$3 # Path to the base directory where adapter clipped fastq file are stored in a subdirectory titled "adapter_clipped" and into which output files will be written to separate subdirectories. An example for the Greenland cod data is: /workdir/cod/greenland-cod
 DATATYPE=$4 # Data type. pe for paired end data and se for single end data. 
+FILTER=$5 # Type of filtering. Values can be: polyg (forced PolyG trimming only), quality (quality trimming, PolyG will be trimmed as well if processing NextSeq/NovaSeq data) 
 
 ## Loop over each sample
 for SAMPLEFILE in `cat $SAMPLELIST`; do
@@ -22,12 +23,19 @@ SAMPLEQUAL=$BASEDIR'qual_filtered/'$SAMPLE_SEQ_ID
 ## Trim polyg tail with fastp. Can also do quality and length trimming.
 # -Q disables quality filter, -L disables length filter, -A disables adapter trimming
 # Go to https://github.com/OpenGene/fastp for more information
-if [ $DATATYPE = pe ]; then
-/programs/fastp/fastp --trim_poly_g -Q -L -A -i $SAMPLEADAPT'_adapter_clipped_f_paired.fastq.gz' -I $SAMPLEADAPT'_adapter_clipped_r_paired.fastq.gz' -o $SAMPLEQUAL'_adapter_clipped_qual_filtered_f_paired.fastq.gz' -O $SAMPLEQUAL'_adapter_clipped_qual_filtered_r_paired.fastq.gz'
-
+if [ $DATATYPE = pe ]
+then
+	if [ $FILTER = polyg ]; then
+		/programs/fastp/fastp --trim_poly_g -Q -L -A -i $SAMPLEADAPT'_adapter_clipped_f_paired.fastq.gz' -I $SAMPLEADAPT'_adapter_clipped_r_paired.fastq.gz' -o $SAMPLEQUAL'_adapter_clipped_qual_filtered_f_paired.fastq.gz' -O $SAMPLEQUAL'_adapter_clipped_qual_filtered_r_paired.fastq.gz'
+	else [ $FILTER = quality ]; then		
+		/programs/fastp/fastp -L -A -i $SAMPLEADAPT'_adapter_clipped_f_paired.fastq.gz' -I $SAMPLEADAPT'_adapter_clipped_r_paired.fastq.gz' -o $SAMPLEQUAL'_adapter_clipped_qual_filtered_f_paired.fastq.gz' -O $SAMPLEQUAL'_adapter_clipped_qual_filtered_r_paired.fastq.gz'
+	fi
 else [ $DATATYPE = se ]
-#/programs/fastp/fastp --trim_poly_g -Q -L -A -i $SAMPLEADAPT'_adapter_clipped_se.fastq.gz' -o $SAMPLEQUAL'_adapter_clipped_qual_filtered_se.fastq.gz' 
-
+	if [ $FILTER = polyg ]; then
+		/programs/fastp/fastp --trim_poly_g -Q -L -A -i $SAMPLEADAPT'_adapter_clipped_se.fastq.gz' -o $SAMPLEQUAL'_adapter_clipped_qual_filtered_se.fastq.gz' 
+	else [ $FILTER = quality ]; then		
+		/programs/fastp/fastp -L -A -i $SAMPLEADAPT'_adapter_clipped_se.fastq.gz' -o $SAMPLEQUAL'_adapter_clipped_qual_filtered_se.fastq.gz' 
+	fi
 fi
 
 done
