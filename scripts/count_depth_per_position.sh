f9ee9f94dddc9cff6d0999ae3014e7e93b39cfa3
Nicolas Lou <nicolas931010@hotmail.com>
2019-03-28 10:10:09 -0400

    added starting column option

--- scripts/count_depth_per_position.sh
+++ scripts/count_depth_per_position.sh
@@ -5,10 +5,10 @@ BAMLIST=$1 # Path to a list of merged bam files. Full paths should be included. 
 BASEDIR=$2 # Path to the base directory where adapter clipped fastq file are stored in a subdirectory titled "adapter_clipped" and into which output files will be written to separate subdirectories. An example for the Greenland cod data is: /workdir/cod/greenland-cod
 
 ## Count per position depth per sample
-samtools depth `cat $BAMLIST` > $BASEDIR'sample_lists/depth_per_position_per_sample.tsv'
+#samtools depth `cat $BAMLIST` > $BASEDIR'sample_lists/depth_per_position_per_sample.tsv'
 ## Convert depth to presence/absence
-awk -f convert_count_to_presence.awk `cut --complement -f 1,2 $BASEDIR'sample_lists/depth_per_position_per_sample.tsv'` > $BASEDIR'sample_lists/presence_per_position_per_sample.tsv'
+awk -v column_to_start=3 -f /workdir/data-processing/scripts/convert_count_to_presence.awk $BASEDIR'sample_lists/depth_per_position_per_sample.tsv' > $BASEDIR'sample_lists/presence_per_position_per_sample.tsv'
 ## Count sum of per position depth over all samples
-awk -f /workdir/data-processing/scripts/sum_by_row.awk `cut --complement -f 1,2 $BASEDIR'sample_lists/depth_per_position_per_sample.tsv'` > $BASEDIR'sample_lists/depth_per_position_sum_over_all_samples.tsv'
+awk -v column_to_start=3 -f /workdir/data-processing/scripts/sum_by_row.awk $BASEDIR'sample_lists/depth_per_position_per_sample.tsv' > $BASEDIR'sample_lists/depth_per_position_sum_over_all_samples.tsv'
 ## Count sum of per position presence over all samples
-awk -f /workdir/data-processing/scripts/sum_by_row.awk $BASEDIR'sample_lists/presence_per_position_per_sample.tsv' > $BASEDIR'sample_lists/presence_per_position_sum_over_all_samples.tsv'
+awk -v column_to_start=3 -f /workdir/data-processing/scripts/sum_by_row.awk $BASEDIR'sample_lists/presence_per_position_per_sample.tsv' > $BASEDIR'sample_lists/presence_per_position_sum_over_all_samples.tsv'
