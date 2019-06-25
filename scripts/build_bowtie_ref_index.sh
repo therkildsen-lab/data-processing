36a443b4ff184a53cb118a0a2fe24d7b7a73083a
Nicolas Lou <nicolas931010@hotmail.com>
2019-06-25 13:26:00 -0400

    Added a step to create .bai and .dict files

--- scripts/build_bowtie_ref_index.sh
+++ scripts/build_bowtie_ref_index.sh
@@ -6,6 +6,16 @@
 REFERENCE=$1 # path to reference fasta file and file name, e.g /workdir/cod/reference_seqs/gadMor2.fasta
 REFNAME=$2 # reference name to add to output files, e.g. gadMor2
 
-## Build the reference index
 REFBASENAME="${REFERENCE%.*}"
+
+## First create .bai and .dict files if they haven't been created
+if [ ! -f $REFERENCE'.fai' ] ; then
+	samtools faidx $REFERENCE
+fi
+
+if [ ! -f $REFBASENAME'.dict' ] ; then
+	java -jar /programs/picard-tools-2.19.2/picard.jar CreateSequenceDictionary R=$REFERENCE O=$REFBASENAME'.dict'
+fi
+
+## Build the reference index
 bowtie2-build $REFERENCE $REFBASENAME
