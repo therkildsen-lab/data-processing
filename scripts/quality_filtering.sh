7bba7e35c8f08f5b54201a45f4866fb4d0439e7e
Nicolas Lou <nicolas931010@hotmail.com>
2021-01-12 21:42:54 -0500

    Update quality_filtering.sh
    soft coded number of cores and fastq path, use cut_right in addition to trim_poly_g for polyG trimming, and use a simple quality trimming for "quality" option

--- scripts/quality_filtering.sh
+++ scripts/quality_filtering.sh
@@ -5,7 +5,9 @@ SAMPLELIST=$1 # Path to a list of prefixes of the raw fastq files. It should be 
 SAMPLETABLE=$2 # Path to a sample table where the 1st column is the prefix of the raw fastq files. The 4th column is the sample ID, the 2nd column is the lane number, and the 3rd column is sequence ID. The combination of these three columns have to be unique. The 6th column should be data type, which is either pe or se. An example of such a sample table is: /workdir/cod/greenland-cod/sample_lists/sample_table.tsv
 BASEDIR=$3 # Path to the base directory where adapter clipped fastq file are stored in a subdirectory titled "adapter_clipped" and into which output files will be written to separate subdirectories. An example for the Greenland cod data is: /workdir/cod/greenland-cod
 FILTER=$4 # Type of filtering. Values can be: polyg (forced PolyG trimming only), quality (quality trimming, PolyG will be trimmed as well if processing NextSeq/NovaSeq data), or length (trim all reads to a maximum length)
-MAXLENGTH=${5:-100} # Maximum length. This input is only relevant when FILTER=length, and its default value is 100.
+THREAD=${5:-10} # Number of thread to use. Default is 10
+FASTP=${6:-/workdir/programs/fastp_0.19.7/fastp} ## Path to the fastp program. The default path is /workdir/programs/fastp_0.19.7/fastp
+MAXLENGTH=${7:-100} # Maximum length. This input is only relevant when FILTER=length, and its default value is 100.
 
 ## Loop over each sample
 for SAMPLEFILE in `cat $SAMPLELIST`; do
@@ -24,24 +26,24 @@ for SAMPLEFILE in `cat $SAMPLELIST`; do
 	SAMPLEQUAL=$BASEDIR'qual_filtered/'$SAMPLE_SEQ_ID
 	
 	## Trim polyg tail or low quality tail with fastp.
-	# --trim_poly_g forces polyg trimming, -r enables cut_right quality trimming
+	# --trim_poly_g forces polyg trimming, --cut_right enables cut_right quality trimming
 	# -Q disables quality filter, -L disables length filter, -A disables adapter trimming
 	# Go to https://github.com/OpenGene/fastp for more information
 	if [ $DATATYPE = pe ]; then
 		if [ $FILTER = polyg ]; then
-			/workdir/programs/fastp --trim_poly_g -Q -L -A -i $SAMPLEADAPT'_adapter_clipped_f_paired.fastq.gz' -I $SAMPLEADAPT'_adapter_clipped_r_paired.fastq.gz' -o $SAMPLEQUAL'_adapter_clipped_qual_filtered_f_paired.fastq.gz' -O $SAMPLEQUAL'_adapter_clipped_qual_filtered_r_paired.fastq.gz'
+			$FASTP --trim_poly_g --cut_right -L -A --thread $THREAD -i $SAMPLEADAPT'_adapter_clipped_f_paired.fastq.gz' -I $SAMPLEADAPT'_adapter_clipped_r_paired.fastq.gz' -o $SAMPLEQUAL'_adapter_clipped_qual_filtered_f_paired.fastq.gz' -O $SAMPLEQUAL'_adapter_clipped_qual_filtered_r_paired.fastq.gz' -h $SAMPLEQUAL'_adapter_clipped_fastp.html' -j $SAMPLEQUAL'_adapter_clipped_fastp.json'
 		elif [ $FILTER = quality ]; then
-			/workdir/programs/fastp --cut_right --cut_window_size 10 --cut_mean_quality 15 -L -Q -A -i $SAMPLEADAPT'_adapter_clipped_f_paired.fastq.gz' -I $SAMPLEADAPT'_adapter_clipped_r_paired.fastq.gz' -o $SAMPLEQUAL'_adapter_clipped_qual_filtered_f_paired.fastq.gz' -O $SAMPLEQUAL'_adapter_clipped_qual_filtered_r_paired.fastq.gz'
+			$FASTP -L -A --thread $THREAD -i $SAMPLEADAPT'_adapter_clipped_f_paired.fastq.gz' -I $SAMPLEADAPT'_adapter_clipped_r_paired.fastq.gz' -o $SAMPLEQUAL'_adapter_clipped_qual_filtered_f_paired.fastq.gz' -O $SAMPLEQUAL'_adapter_clipped_qual_filtered_r_paired.fastq.gz' -h $SAMPLEQUAL'_adapter_clipped_fastp.html' -j $SAMPLEQUAL'_adapter_clipped_fastp.json'
 		elif [ $FILTER = length ]; then
-			/workdir/programs/fastp --max_len1 $MAXLENGTH -Q -L -A -i $SAMPLEADAPT'_adapter_clipped_f_paired.fastq.gz' -I $SAMPLEADAPT'_adapter_clipped_r_paired.fastq.gz' -o $SAMPLEQUAL'_adapter_clipped_qual_filtered_f_paired.fastq.gz' -O $SAMPLEQUAL'_adapter_clipped_qual_filtered_r_paired.fastq.gz'
+			$FASTP --max_len1 $MAXLENGTH -Q -L -A --thread $THREAD -i $SAMPLEADAPT'_adapter_clipped_f_paired.fastq.gz' -I $SAMPLEADAPT'_adapter_clipped_r_paired.fastq.gz' -o $SAMPLEQUAL'_adapter_clipped_qual_filtered_f_paired.fastq.gz' -O $SAMPLEQUAL'_adapter_clipped_qual_filtered_r_paired.fastq.gz' -h $SAMPLEQUAL'_adapter_clipped_fastp.html' -j $SAMPLEQUAL'_adapter_clipped_fastp.json'
 		fi
 	elif [ $DATATYPE = se ]; then
 		if [ $FILTER = polyg ]; then
-			/workdir/programs/fastp --trim_poly_g -Q -L -A -i $SAMPLEADAPT'_adapter_clipped_se.fastq.gz' -o $SAMPLEQUAL'_adapter_clipped_qual_filtered_se.fastq.gz' 
+			$FASTP --trim_poly_g --cut_right -L -A --thread $THREAD -i $SAMPLEADAPT'_adapter_clipped_se.fastq.gz' -o $SAMPLEQUAL'_adapter_clipped_qual_filtered_se.fastq.gz' -h $SAMPLEQUAL'_adapter_clipped_fastp.html' -j $SAMPLEQUAL'_adapter_clipped_fastp.json'
 		elif [ $FILTER = quality ]; then
-			/workdir/programs/fastp --cut_right --cut_window_size 10 --cut_mean_quality 15 -L -A -i $SAMPLEADAPT'_adapter_clipped_se.fastq.gz' -o $SAMPLEQUAL'_adapter_clipped_qual_filtered_se.fastq.gz' 
+			$FASTP -L -A --thread $THREAD -i $SAMPLEADAPT'_adapter_clipped_se.fastq.gz' -o $SAMPLEQUAL'_adapter_clipped_qual_filtered_se.fastq.gz' -h $SAMPLEQUAL'_adapter_clipped_fastp.html' -j $SAMPLEQUAL'_adapter_clipped_fastp.json'
 		elif [ $FILTER = length ]; then
-			/workdir/programs/fastp --max_len1 $MAXLENGTH -Q -L -A -i $SAMPLEADAPT'_adapter_clipped_se.fastq.gz' -o $SAMPLEQUAL'_adapter_clipped_qual_filtered_se.fastq.gz' 
+			$FASTP --max_len1 $MAXLENGTH -Q -L -A --thread $THREAD -i $SAMPLEADAPT'_adapter_clipped_se.fastq.gz' -o $SAMPLEQUAL'_adapter_clipped_qual_filtered_se.fastq.gz' -h $SAMPLEQUAL'_adapter_clipped_fastp.html' -j $SAMPLEQUAL'_adapter_clipped_fastp.json'
 		fi
 	fi
 	
