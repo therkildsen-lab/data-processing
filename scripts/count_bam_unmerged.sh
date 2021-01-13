525affcfe3be658a8cf0e7e489faac202fd3f13c
Nicolas Lou <nicolas931010@hotmail.com>
2021-01-13 15:28:09 -0500

    Update count_bam_unmerged.sh
    soft-coded samtools path and made it optional to count the sorted bam file

--- scripts/count_bam_unmerged.sh
+++ scripts/count_bam_unmerged.sh
@@ -4,8 +4,14 @@ SAMPLELIST=$1 # Path to a list of prefixes of the raw fastq files. It should be 
 SAMPLETABLE=$2 # Path to a sample table where the 1st column is the prefix of the raw fastq files. The 4th column is the sample ID, the 2nd column is the lane number, and the 3rd column is sequence ID. The combination of these three columns have to be unique. The 6th column should be data type, which is either pe or se. An example of such a sample table is: /workdir/cod/greenland-cod/sample_lists/sample_table.tsv
 BASEDIR=$3 # Path to the base directory where adapter clipped fastq file are stored in a subdirectory titled "adapter_clipped" and into which output files will be written to separate subdirectories. An example for the Greenland cod data is: /workdir/cod/greenland-cod/
 REFNAME=$4 # Reference name to add to output files, e.g. gadMor2
+SAMTOOLS=${5:-samtools} # Path to samtools
+MINQ=$6 # Minimum mapping quality filter used in the sorting step. When left undefined, the sorted bam file will not be counted (e.g. when reads are not filtered in the sorting step).
 
-printf 'sample_id\tmapped_bases\tqual_filtered_mapped_bases\n'
+if [ -z "$MINQ" ]; then
+	printf 'sample_id\tmapped_bases\n'
+else
+	printf 'sample_id\tmapped_bases\tqual_filtered_mapped_bases\n'
+fi
 
 for SAMPLEFILE in `cat $SAMPLELIST`; do
 	
@@ -20,12 +26,15 @@ for SAMPLEFILE in `cat $SAMPLELIST`; do
 	
 	## Count raw mapped bases
 	RAWBAMFILE=$BASEDIR'bam/'$SAMPLE_SEQ_ID'_'$DATATYPE'_bt2_'$REFNAME'.bam'
-	MAPPEDBASES=`samtools stats $RAWBAMFILE | grep ^SN | cut -f 2- | grep "^bases mapped (cigar)" | cut -f 2`
+	MAPPEDBASES=`$SAMTOOLS stats $RAWBAMFILE | grep ^SN | cut -f 2- | grep "^bases mapped (cigar)" | cut -f 2`
 	
-	## Count quality filtered mapped bases
-	QUALFILTBAMFILE=$BASEDIR'bam/'$SAMPLE_SEQ_ID'_'$DATATYPE'_bt2_'$REFNAME'_minq20_sorted.bam'
-	QUAFILTBASES=`samtools stats $QUALFILTBAMFILE | grep ^SN | cut -f 2- | grep "^bases mapped (cigar)" | cut -f 2`
+	if [ -z "$MINQ" ]; then
+		printf "%s\t%s\n" $SAMPLE_SEQ_ID $MAPPEDBASES
+	else
+		## Count quality filtered mapped bases
+		QUALFILTBAMFILE=$BASEDIR'bam/'$SAMPLE_SEQ_ID'_'$DATATYPE'_bt2_'$REFNAME'_minq'$MINQ'_sorted.bam'
+		QUAFILTBASES=`$SAMTOOLS stats $QUALFILTBAMFILE | grep ^SN | cut -f 2- | grep "^bases mapped (cigar)" | cut -f 2`
 	
-	printf "%s\t%s\t%s\n" $SAMPLE_SEQ_ID $MAPPEDBASES $QUAFILTBASES
-	
+		printf "%s\t%s\t%s\n" $SAMPLE_SEQ_ID $MAPPEDBASES $QUAFILTBASES
+	fi
 done
