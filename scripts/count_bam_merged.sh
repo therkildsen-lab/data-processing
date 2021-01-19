3100cf593c46360334d2cc9868726d72533c0714
Nicolas Lou <nicolas931010@hotmail.com>
2021-01-18 20:21:30 -0500

    debugging

--- scripts/count_bam_merged.sh
+++ scripts/count_bam_merged.sh
@@ -3,7 +3,8 @@
 # This script is used to count bam files after merging. These include deduplication and overlap clipping (paired-end data only)
 BAMLIST=$1 # Path to a list of merged bam files. Full paths should be included. An example of such a bam list is /workdir/cod/greenland-cod/sample_lists/bam_list_merged.tsv
 SAMPLETABLE=$2 # Path to a sample table where the 1st column is the prefix of the MERGED bam files. The 4th column is the sample ID, the 2nd column is the lane number, and the 3rd column is sequence ID. The 5th column is population name and 6th column is the data type. An example of such a sample table is: /workdir/cod/greenland-cod/sample_lists/sample_table_merged.tsv
-MINMAPQ=$3 # Minimum mapping quality filter (can be blank, in which case the filtered won't be applied)
+SAMTOOLS=${3:-samtools} # Path to samtools
+MINMAPQ=$4 # Minimum mapping quality filter (can be blank, in which case the filtered won't be applied)
 
 if [ ! -z "$MINMAPQ" ]; then
 	printf 'sample_id\tdedup_mapped_bases\tavg_fragment_size\toverlap_clipped_bases\tminmapq'$MINMAPQ'_bases\n'
@@ -19,37 +20,37 @@ for SAMPLEBAM in `cat $BAMLIST`; do
 	
 	## Count deduplicated bases
 	DEDUPFILE=$SAMPLEPREFIX'_dedup.bam'
-	DEDUPMAPPEDBASES=`samtools stats $DEDUPFILE -@ 4 | grep ^SN | cut -f 2- | grep "^bases mapped (cigar)" | cut -f 2`
+	DEDUPMAPPEDBASES=`$SAMTOOLS stats $DEDUPFILE -@ 4 | grep ^SN | cut -f 2- | grep "^bases mapped (cigar)" | cut -f 2` &
 	
 	## Extract data type from the merged sample table
 	DATATYPE=`grep -P "${SAMPLESEQID}\t" $SAMPLETABLE | cut -f 6`
 	
 	if [ $DATATYPE != se ]; then
 		## Calculate average fragment length for paired end reads
-		AVGFRAG=`samtools view $DEDUPFILE | grep YT:Z:CP | awk '{sum+=sqrt($9^2)} END {printf "%f", sum/NR}'`
+		AVGFRAG=`$SAMTOOLS view $DEDUPFILE | grep YT:Z:CP | awk '{sum+=sqrt($9^2)} END {printf "%f", sum/NR}'` &
 		if [ "$AVGFRAG" == '' ]; then AVGFRAG=0 ; fi
 	
 		## Count overlap clipped bam files for paired end reads 
 		CLIPOVERLAPFILE=$SAMPLEPREFIX'_dedup_overlapclipped.bam'
-		CLIPOVERLAPBASES=`samtools stats $CLIPOVERLAPFILE -@ 4 | grep ^SN | cut -f 2- | grep "^bases mapped (cigar)" | cut -f 2`
+		CLIPOVERLAPBASES=`$SAMTOOLS stats $CLIPOVERLAPFILE -@ 4 | grep ^SN | cut -f 2- | grep "^bases mapped (cigar)" | cut -f 2` &
 		
 		if [ ! -z "$MINMAPQ" ]; then
-			MINMAPQBASES=`samtools view -q $MINMAPQ $CLIPOVERLAPFILE -@ 4 | samtools stats -@ 4| grep ^SN | cut -f 2- | grep "^bases mapped (cigar)" | cut -f 2`
+			MINMAPQBASES=`$SAMTOOLS view -q $MINMAPQ $CLIPOVERLAPFILE -@ 4 | $SAMTOOLS stats -@ 4| grep ^SN | cut -f 2- | grep "^bases mapped (cigar)" | cut -f 2` &
 		fi
 	else
 		AVGFRAG=NA
 		CLIPOVERLAPBASES=NA
 		if [ ! -z "$MINMAPQ" ]; then
-			MINMAPQBASES=`samtools view -q $MINMAPQ $DEDUPFILE -@ 4 | samtools stats -@ 4| grep ^SN | cut -f 2- | grep "^bases mapped (cigar)" | cut -f 2`
+			MINMAPQBASES=`$SAMTOOLS view -q $MINMAPQ $DEDUPFILE -@ 4 | $SAMTOOLS stats -@ 4| grep ^SN | cut -f 2- | grep "^bases mapped (cigar)" | cut -f 2` &
 		fi
 	fi
 	
+	wait 
+
 	if [ ! -z "$MINMAPQ" ]; then
-		printf "%s\t%s\t%s\t%s\n" $SAMPLEPREFIX $DEDUPMAPPEDBASES $AVGFRAG $CLIPOVERLAPBASES $MINMAPQBASES
+		printf "%s\t%s\t%s\t%s\t%s\n" $SAMPLEPREFIX $DEDUPMAPPEDBASES $AVGFRAG $CLIPOVERLAPBASES $MINMAPQBASES
 	else
 		printf "%s\t%s\t%s\t%s\n" $SAMPLEPREFIX $DEDUPMAPPEDBASES $AVGFRAG $CLIPOVERLAPBASES
 	fi
 
-	
-	
 done
