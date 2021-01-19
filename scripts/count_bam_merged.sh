fd2066eb30d203d37f89d716d36d8280902efb38
Nicolas Lou <nicolas931010@hotmail.com>
2021-01-18 20:09:41 -0500

    debugging
    no longer backward compatible

--- scripts/count_bam_merged.sh
+++ scripts/count_bam_merged.sh
@@ -3,9 +3,7 @@
 # This script is used to count bam files after merging. These include deduplication and overlap clipping (paired-end data only)
 BAMLIST=$1 # Path to a list of merged bam files. Full paths should be included. An example of such a bam list is /workdir/cod/greenland-cod/sample_lists/bam_list_merged.tsv
 SAMPLETABLE=$2 # Path to a sample table where the 1st column is the prefix of the MERGED bam files. The 4th column is the sample ID, the 2nd column is the lane number, and the 3rd column is sequence ID. The 5th column is population name and 6th column is the data type. An example of such a sample table is: /workdir/cod/greenland-cod/sample_lists/sample_table_merged.tsv
-BASEDIR=$3 # Path to the base directory where adapter clipped fastq file are stored in a subdirectory titled "adapter_clipped" and into which output files will be written to separate subdirectories. An example for the Greenland cod data is: /workdir/cod/greenland-cod/
-REFNAME=$4 # Reference name to add to output files, e.g. gadMor2
-MINMAPQ=$5 # Minimum mapping quality filter (can be blank, in which case the filtered won't be applied)
+MINMAPQ=$3 # Minimum mapping quality filter (can be blank, in which case the filtered won't be applied)
 
 if [ ! -z "$MINMAPQ" ]; then
 	printf 'sample_id\tdedup_mapped_bases\tavg_fragment_size\toverlap_clipped_bases\tminmapq'$MINQ'_bases\n'
@@ -16,14 +14,15 @@ fi
 for SAMPLEBAM in `cat $BAMLIST`; do
 	
 	## Extract the file name prefix for this sample
-	SAMPLEPREFIX=`echo $SAMPLEBAM | sed 's/_bt2_.*//' | sed -e 's#.*/bam/\(\)#\1#'`
+	SAMPLESEQID=`echo $SAMPLEBAM | sed 's/_bt2_.*//' | sed -e 's#.*/bam/\(\)#\1#'`
+	SAMPLEPREFIX=`echo ${SAMPLEBAM%.bam}`
 	
 	## Count deduplicated bases
-	DEDUPFILE=$BASEDIR'bam/'$SAMPLEPREFIX'_bt2_'$REFNAME'_minq20_sorted_dedup.bam'
+	DEDUPFILE=$SAMPLEPREFIX'_dedup.bam'
 	DEDUPMAPPEDBASES=`samtools stats $DEDUPFILE -@ 4 | grep ^SN | cut -f 2- | grep "^bases mapped (cigar)" | cut -f 2`
 	
 	## Extract data type from the merged sample table
-	DATATYPE=`grep -P "${SAMPLEPREFIX}\t" $SAMPLETABLE | cut -f 6`
+	DATATYPE=`grep -P "${SAMPLESEQID}\t" $SAMPLETABLE | cut -f 6`
 	
 	if [ $DATATYPE != se ]; then
 		## Calculate average fragment length for paired end reads
@@ -31,7 +30,7 @@ for SAMPLEBAM in `cat $BAMLIST`; do
 		if [ "$AVGFRAG" == '' ]; then AVGFRAG=0 ; fi
 	
 		## Count overlap clipped bam files for paired end reads 
-		CLIPOVERLAPFILE=$BASEDIR'bam/'$SAMPLEPREFIX'_bt2_'$REFNAME'_minq20_sorted_dedup_overlapclipped.bam'
+		CLIPOVERLAPFILE=$SAMPLEPREFIX'_dedup_overlapclipped.bam'
 		CLIPOVERLAPBASES=`samtools stats $CLIPOVERLAPFILE -@ 4 | grep ^SN | cut -f 2- | grep "^bases mapped (cigar)" | cut -f 2`
 		
 		if [ ! -z "$MINMAPQ" ]; then
