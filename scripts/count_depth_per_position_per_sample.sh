134c6ca8f42ec45d90155b5afd196884bfa8b16b
Nicolas Lou <nicolas931010@hotmail.com>
2021-02-18 14:30:14 -0500

    added annotation

--- scripts/count_depth_per_position_per_sample.sh
+++ scripts/count_depth_per_position_per_sample.sh
@@ -3,10 +3,10 @@
 ## This script is used to count per position depth for bam files. It will create one depth file per bam file.
 # Advantage of this version is that you can use R to process the depth files.
 BAMLIST=$1 # Path to a list of merged bam files. Full paths should be included. An example of such a bam list is /workdir/cod/greenland-cod/sample_lists/bam_list_merged.tsv
-JOBS=${2:-1}
-MINBASEQ=${3:-20}
-MINMAPQ=${4:-20}
-SAMTOOLS=${5:-samtools}
+JOBS=${2:-1} # Number of parallel jobs to run simultaneously
+MINBASEQ=${3:-20} # Minimum base quality filter
+MINMAPQ=${4:-20} # Minimum mapping quality filter
+SAMTOOLS=${5:-samtools} # Path to Samtools
 
 JOB_INDEX=0
 
