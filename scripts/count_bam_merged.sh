0d1b5185a5d02991c7f609cce4c1b9e5193d7151
R. Nicolas Lou <nicolas931010@hotmail.com>
2021-08-20 17:30:52 -0400

    made THREADS customizable

--- scripts/count_bam_merged.sh
+++ scripts/count_bam_merged.sh
@@ -5,6 +5,7 @@ BAMLIST=$1 # Path to a list of merged bam files. Full paths should be included. 
 SAMPLETABLE=$2 # Path to a sample table where the 1st column is the prefix of the MERGED bam files. The 4th column is the sample ID, the 2nd column is the lane number, and the 3rd column is sequence ID. The 5th column is population name and 6th column is the data type. An example of such a sample table is: /workdir/cod/greenland-cod/sample_lists/sample_table_merged.tsv
 SAMTOOLS=${3:-samtools} # Path to samtools
 MINMAPQ=${4-0} # Minimum mapping quality filter (the default is 0, in which case the filter won't be applied)
+THREADS=${5-1} # Number of parallel threads to use. Default is 1.
 
 if [ $MINMAPQ != 0 ]; then
 	printf 'sample_id\tdedup_mapped_bases\tavg_fragment_size\toverlap_clipped_bases\tminmapq'$MINMAPQ'_bases\n'
@@ -20,7 +21,7 @@ for SAMPLEBAM in `cat $BAMLIST`; do
 	
 	## Count deduplicated bases
 	DEDUPFILE=$SAMPLEPREFIX'_dedup.bam'
-	DEDUPMAPPEDBASES=`$SAMTOOLS stats $DEDUPFILE -@ 4 | grep ^SN | cut -f 2- | grep "^bases mapped (cigar)" | cut -f 2`
+	DEDUPMAPPEDBASES=`$SAMTOOLS stats $DEDUPFILE -@ $THREADS | grep ^SN | cut -f 2- | grep "^bases mapped (cigar)" | cut -f 2`
 	
 	## Extract data type from the merged sample table
 	DATATYPE=`grep -P "${SAMPLESEQID}\t" $SAMPLETABLE | cut -f 6`
@@ -32,16 +33,16 @@ for SAMPLEBAM in `cat $BAMLIST`; do
 	
 		## Count overlap clipped bam files for paired end reads 
 		CLIPOVERLAPFILE=$SAMPLEPREFIX'_dedup_overlapclipped.bam'
-		CLIPOVERLAPBASES=`$SAMTOOLS stats $CLIPOVERLAPFILE -@ 4 | grep ^SN | cut -f 2- | grep "^bases mapped (cigar)" | cut -f 2`
+		CLIPOVERLAPBASES=`$SAMTOOLS stats $CLIPOVERLAPFILE -@ $THREADS | grep ^SN | cut -f 2- | grep "^bases mapped (cigar)" | cut -f 2`
 		
 		if [ $MINMAPQ != 0 ]; then
-			MINMAPQBASES=`$SAMTOOLS view -h -q $MINMAPQ $CLIPOVERLAPFILE -@ 4 | $SAMTOOLS stats -@ 4| grep ^SN | cut -f 2- | grep "^bases mapped (cigar)" | cut -f 2`
+			MINMAPQBASES=`$SAMTOOLS view -h -q $MINMAPQ $CLIPOVERLAPFILE -@ $THREADS | $SAMTOOLS stats -@ $THREADS | grep ^SN | cut -f 2- | grep "^bases mapped (cigar)" | cut -f 2`
 		fi
 	else
 		AVGFRAG=NA
 		CLIPOVERLAPBASES=NA
 		if [ $MINMAPQ != 0 ]; then
-			MINMAPQBASES=`$SAMTOOLS view -h -q $MINMAPQ $DEDUPFILE -@ 4 | $SAMTOOLS stats -@ 4| grep ^SN | cut -f 2- | grep "^bases mapped (cigar)" | cut -f 2`
+			MINMAPQBASES=`$SAMTOOLS view -h -q $MINMAPQ $DEDUPFILE -@ $THREADS | $SAMTOOLS stats -@ $THREADS | grep ^SN | cut -f 2- | grep "^bases mapped (cigar)" | cut -f 2`
 		fi
 	fi
 	
